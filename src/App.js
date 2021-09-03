1226f405c3c7963de7024064e67d2eb485595706
Kaalaayaa <kalaya.chatelain@yahoo.fr>
2021-09-03 14:42:21 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+export default function App() {
+    return(
+        <div>
+            
+            <div class="attribution">
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+                Coded by <a href="#">Your Name Here</a>.
+            </div>
+        </div>
+    )
+}
